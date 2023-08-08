000b30eef9d1dfab2e63e1c7031d5bf7a6753281
ETINAweb237 <petitdassi7@gmail.com>
2023-08-08 14:19:02 +0100

    modification des fichier

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+    try {
+        //connexion a la base de donnees
+        $db= new PDO("mysql:host=localhost; dbname=test; port=3306; chartset=utf8",'root','');
+
+    } catch (PDOExeption $th) {
+        // en cas d'echec de connexion a la base de donnees 
+        echo 'Erreur '.$th->getMessage();
+        die();
+    }
+?>
---
+++ inscription.php
@@ -0,0 +1,5 @@
+<?php
+//require once le fichier conect pour la connexion a la base de dennees
+require_once('connect.php');
+
+?>
