f6225490cbf63dac2901799a8297c114d2ee0893
ETINAweb237 <petitdassi7@gmail.com>
2023-08-09 08:19:53 +0100

    modification de la base de donnees

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
     try {
         //connexion a la base de donnees
-        $db= new PDO("mysql:host=localhost; dbname=test; port=3306; chartset=utf8",'root','');
+        $db= new PDO("mysql:host=localhost; dbname=sir_gestion; port=3306; chartset=utf8",'root','');
 
     } catch (PDOExeption $th) {
         // en cas d'echec de connexion a la base de donnees 
